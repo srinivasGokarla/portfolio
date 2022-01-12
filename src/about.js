7dc6d668560005b9c66ff149a57d4c05cd150797
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-12 09:59:57 +0530

    again

--- src/about.js
+++ src/about.js
@@ -15,10 +15,13 @@ export default function About() {
 
           <div className="para">
             <p>
+          Meticulous Full Stack Web Developer dedicated to developing clean and scalable code.
+          Passionate about creating the wonderful digital and overall great user experience.Effective Communicator and a learning enthusiast with a proactive attitude ready to take up tasks with a commitment to professionalism.
+            <br></br>
             Strong in design and integration with intuitive problem-solving skills.
-Passionate about implementing and launching new projects. Looking to
-start a career as a Full-Stack web developer with a reputed firm driven by
-technology
+            Passionate about implementing and launching new projects. Looking to
+           start a career as a Full-Stack web developer with a reputed firm driven by
+            technology.
             </p>
           </div>
         </div>
