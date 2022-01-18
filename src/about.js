bf97b435f704a09c9d4bd297108021b98868e9f1
srinivas217 <87422030+srinu217@users.noreply.github.com>
2022-01-18 14:58:12 +0530

    updated

--- src/about.js
+++ src/about.js
@@ -16,8 +16,7 @@ export default function About() {
           <div className="para">
        
             <p >
-            I’m a full-stack web developer and creative coder who specializes in MERN stack development.My mission is to translate user-focused designs into pixel-perfect websites or applications that run blazing fast. A passionate learner who's always willing to learn and work across technologies and domains 💡.
-I love to explore new technologies and leverage them to solve real-life problems.
+            I’m a full-stack web developer and creative coder who specializes in MERN stack development. My mission is to translate user-focused designs into pixel-perfect websites or applications that run blazing fast. A passionate learner who's always willing to learn and work across technologies and domains. I love to explore new technologies and leverage them to solve real-life problems.
             <br />
             <br />I am a Mechanical Engineer Who Fell in Love
            
