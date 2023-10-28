bfac1d08a57fc9ef3dfee97d56ad54bba2ccb36f
srinivasGokarla <srinivasgokarla@gmail.com>
2023-10-28 08:51:05 +0530

    updated portfolio

--- js/main.js
+++ js/main.js
@@ -53,11 +53,7 @@
    $(".some-activities").html('<span class="highlight-key">Company Name :</span> DxMinds Innovation Labs Pvt. Ltd <br><span class="highlight-key">Duration :</span> JUN 2023 to Until Now <br><span class="highlight-key">Tech Stacks :</span> HTML | CSS | JavaScript | TypeScript | Node JS | Express JS | ReactJs | MIUI <br><span class="highlight-key">Role :</span> Software Engineer<br><span class="highlight-key">Responsibilities :</span><br>&#128073;  Backend development for "Hallo Club", and "Cozmo" using Node.js, Express.js, MongoDB, TypeScript, and more.<br>&#128073; Created secure authentication systems with CRM and custom APIs for improved platform security and usability.<br>&#128073; Integrated development tools and middleware like logger, Swagger, and npm packages, enhancing project maintainability, and documentation.<br>&#128073; Maintained strong MVC models and a well-structured backend for building model schemas, validations, and authentication.')
   })
 
-  $(".apart-tab").click(function(){
-    $(".apart-tab").addClass('yellow-font')
-    $(".exp-tab").removeClass('yellow-font')
-    $(".some-activities").html('<br>Some other activities that I love to do!<br><span>&#128073; Listening Music</span> <br><span>&#128073; Playing Cricket</span> <br><span>&#128073; Traveling</span><br><br>Live life to the fullest, and focus on the positive. ... "Focus on the journey", not the destination. ..."')
-   })
+ 
     
   var typed = new Typed('.element', {
     strings: ['Full Stack Developer', 'FrontEnd Developer','BackEnd Developer','Full Stack Developer', 'FrontEnd Developer'],
